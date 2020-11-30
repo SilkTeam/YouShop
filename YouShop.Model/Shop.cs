7f37a65d66e53734245167c315cca60aaea22459
Skilemon <35643936+Skilemon@users.noreply.github.com>
2020-11-30 10:26:59 +0800

    Fixed bugs and shop description

--- YouShop.Model/Shop.cs
+++ YouShop.Model/Shop.cs
@@ -12,6 +12,7 @@ namespace YouShop.Model
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Img { get; set; }
         public int SortID { get; set; }
         public bool Status { get; set; }
